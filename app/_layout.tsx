b7283b041ab11d2551bfe241fcd60193b7a4d4a0
1ssethDesu <chhortchhoraseth@gmail.com>
2026-05-05 21:52:23 +0700

    Adding a Sharing Extension

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useShareIntent } from "expo-share-intent";
 import "../global.css";
 
 export default function RootLayout() {
+  const { hasShareIntent, shareIntent, resetShareIntent, error } = useShareIntent();
+
+  useEffect(() => {
+    if (hasShareIntent && shareIntent) {
+      const url = shareIntent.webUrl || shareIntent.text;
+      if (url) {
+        console.log('Shared URL:', url);
+        // We will handle the URL processing here later
+        resetShareIntent();
+      }
+    }
+  }, [hasShareIntent, shareIntent]);
+
   return <Stack />;
 }
